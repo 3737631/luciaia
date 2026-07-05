5dc2af00f3b2864a5d2000ee987e5181edabf232
Paquito <paquito@example.com>
2026-07-05 22:33:21 +0200

    Fix: add basePath and assetPrefix for GitHub Pages subpath

--- next.config.js
+++ next.config.js
@@ -1,10 +1,15 @@
 /** @type {import('next').NextConfig} */
+const repo = "luciaia";
+
 const nextConfig = {
   output: "export",
+  basePath: `/${repo}`,
+  assetPrefix: `/${repo}/`,
   reactStrictMode: true,
   images: {
     unoptimized: true,
   },
+  trailingSlash: true,
 };
 
 module.exports = nextConfig;
