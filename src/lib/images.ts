f81d9e9f409c9e20c25dba7af20aab3cf70989a6
Paquito <paquito@example.com>
2026-07-06 18:41:44 +0200

    fix image paths: add /luciaia basePath prefix

--- src/lib/images.ts
+++ src/lib/images.ts
@@ -1,5 +1,5 @@
 import { HairOption } from "@/data/girls";
 
 export function getGirlImage(girlId: string, hair: HairOption): string {
-  return `/girls/${girlId}-${hair}.jpg`;
+  return `/luciaia/girls/${girlId}-${hair}.jpg`;
 }
